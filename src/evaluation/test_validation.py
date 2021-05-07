1fb95e28480cc2d0636732216d11537f4331c4b0
MartineDeVos <mgdevos@gmail.com>
2021-05-07 16:04:46 +0200

    Add test+data for validation to check consistency of results

--- src/evaluation/test_validation.py
+++ src/evaluation/test_validation.py
@@ -1,24 +1,50 @@
 import pytest
+import pandas as pd
 from pathlib import Path
 from import_files import ImportFiles
-
+from validation_execution import ValidateExecution
+from validation_packagebased import ValidatePackage
 
 @pytest.fixture(autouse=True,scope="class")
 def im_object():
     """Instantiate ImportFiles class with test data; created object is used in all tests""" 
 
-    data_dir = Path("../../data/test_data")
+    data_dir = Path("./test_data")
     processed = data_dir / "processed"
     key_dir = data_dir / "keys"
-    res_file = data_dir / "labeled.json"
+    res_file = data_dir / "result_horses.json"
 
     im = ImportFiles(data_dir,res_file,processed,key_dir)
 
     return im
 
-def test_load_keys(im_object):
+def test_validation(im_object):
+    """Test overall validation procedure for consistency of results""" 
 
-    all_keys = im_object.load_keys()
-    key_dict = all_keys['insta4dummy_20201105']
+    package = "horsesarecool52_20201020"
 
-    assert len(key_dict.keys()) == 9126
+    # Load data: raw, anonymized and ground truth
+    raw_file, result = im_object.load_results(package)    
+    key_files = im_object.load_keys()
+    key_file = key_files[package]
+    package_hashed = key_file[package]
+    anon_text = im_object.open_package(package, package_hashed)
+
+    # Count labels and hashes per file 
+    df_outcome = pd.DataFrame()
+    val_ddp = ValidatePackage(package, anon_text, package_hashed, key_file, result, raw_file)   
+    data_outcome = val_ddp.execute()
+    df_outcome = df_outcome.append(data_outcome, ignore_index=True)
+
+    # Calculate total scores 
+    val_exc = ValidateExecution(Path("./test"), df_outcome)
+    df_val = val_exc.validation()
+    
+    true_pos = df_val['TP'].values
+    # Check totals of true positives
+    test_values = [250,2,252,21,1,22,5,223,228,10,10,76,1,77,22,171,219,3,665,610,1,13,1704]
+    assert (true_pos == test_values).all()
+
+
+
+
